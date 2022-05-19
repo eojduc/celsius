c2fb267fb8177ad163541658a1e60e2b9fcf200e
Del Cudjoe <ervin.cudjoe@duke.edu>
2022-05-19 18:21:58 -0400

    added header and minor styling

--- components/PostPage.js
+++ components/PostPage.js
@@ -1,8 +1,6 @@
-import { StyleSheet, View, Dimensions, Button, ScrollView } from "react-native";
+import { StyleSheet, View, Dimensions, Button, ScrollView, Text } from "react-native";
 import { useRef, useState, useEffect } from "react";
 import PreviewPost from "./PreviewPost";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OpenPost from "./OpenPost";
 import { Modalize } from "react-native-modalize";
 import { collectionGroup, query, onSnapshot } from "firebase/firestore";
@@ -10,7 +8,6 @@ import { firestore } from "../lib/firebase";
 
 const PostPage = ({ color }) => {
   const [openPost, setOpenPost] = useState({});
-  const [modal, setModal] = useState(false);
   const [posts, setPosts] = useState([]);
   const modalizeRef = useRef(null);
   const { width } = Dimensions.get("window");
@@ -28,30 +25,60 @@ const PostPage = ({ color }) => {
     const unsub = onSnapshot(q, (snapshot) => {
       setPosts(snapshot.docs.map((doc) => doc.data()));
     });
+  
 
     return unsub;
   }, []);
-
+  const headerColor = (color) => {
+    switch (color) {
+      case 'red':
+        return 'darksalmon';
+      case 'blue':
+        return 'lightblue';
+      case 'green':
+        return 'lightgreen';
+    }
+  }
   const styles = StyleSheet.create({
-    backgroundColor: color,
-    justifyContent: "center",
-    width: width,
-    flex: 5,
-    paddingTop: 40,
-  });
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+    },
+    content: {
+      backgroundColor: color,
+      justifyContent: "center",
+      width: width,
+      flex: 6,
+    },
+    header: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: headerColor(color),
+      text: {
+        textAlign: 'center',
+      }
+    }
 
+    
+  });
   return (
-    <View key={color.toString()} style={styles}>
-      <Modalize ref={modalizeRef}>
-        <OpenPost color={color} data={openPost} close={closeModal} />
-      </Modalize>
-      <ScrollView vertical>
-        {posts.map((post) => (
-          <PreviewPost data={post} onPress={openModal} />
-        ))}
-      </ScrollView>
-      <Button title="make post" onPress={() => setModal(true)} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.header.text}>HEADER</Text>
+      </View>
+      <View key={color.toString()} style={styles.content}>
+        <Modalize ref={modalizeRef} modalTopOffset={60}>
+          <OpenPost color={color} data={openPost} close={closeModal} />
+        </Modalize>
+        <ScrollView vertical>
+          {posts.map((post) => (
+            <PreviewPost data={post} onPress={openModal} />
+          ))}
+        </ScrollView>
+        <Button title="make post" onPress={() => setModal(true)} />
+      </View>      
     </View>
+
   );
 };
 export default PostPage;
